ddb7948af554dc1f0a65255b1bab4e343bfbcec7
Soumyanil Das <soumyanil666@gmail.com>
2025-02-19 21:17:24 +0530

    Added env

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ init({
     {
       name: 'mf_remote',
       // Adding version to invalidate cache
-      entry: `http://localhost:4202/remoteEntry.js?v=${+Date.now()}`,
+      entry: `${process.env.REMOTE_BASE_URL}/remoteEntry.js?v=${+Date.now()}`,
     },
     // Other remote entries
   ]
